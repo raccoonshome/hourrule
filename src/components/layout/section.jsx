e9fd72189a5527e6eceb9bc6e6a3db5a6b7cbe27
Sleepingoff <justjh30@gmail.com>
2023-09-18 11:54:25 +0900

    modify - rename tagged template function

--- src/components/layout/section.jsx
+++ src/components/layout/section.jsx
@@ -15,7 +15,7 @@ const modalList = [
   },
 ];
 
-const T = (texts, children) => {
+const Template = (texts, children) => {
   return <StyledTemplate>{texts[0]}{children}{texts[1]}</StyledTemplate>
 };
 
@@ -37,8 +37,8 @@ const HourSection = () =>{
   return <DefaultSection>
     {next ? 
       <form>
-        {T`당신은 ${<FontStyles.Mega>{goal}</FontStyles.Mega>} 전문가가 되기 위해서`}
-        {T`대략 ${<FontStyles.Mega>{time}</FontStyles.Mega>} 일 이상 훈련하셔야 됩니다. :)`}
+        {Template`당신은 ${<FontStyles.Mega>{goal}</FontStyles.Mega>} 전문가가 되기 위해서`}
+        {Template`대략 ${<FontStyles.Mega>{time}</FontStyles.Mega>} 일 이상 훈련하셔야 됩니다. :)`}
         <DivButtons>
           <ModalBtn variant={'primary'} value={`훈련하러 가기 GO!GO!`} contents={modalList[0]} />
           <Button  onSubmit={handleClickReset}>다시 계산하기</Button>
@@ -46,8 +46,8 @@ const HourSection = () =>{
       </form>
       :
       <form>
-        {T`나는 ${<><FontStyles.Br/><Input placeholder={`예)프로그래밍`} type={`text`} getText={setGoal}/><FontStyles.Br/></>} 전문가가 될 것이다.`}
-        {T`그래서 앞으로 매일 하루에 ${<><FontStyles.Br/><Input placeholder={`예) 5`} type={`number`} getText={setTime}/><FontStyles.Br/></>} 시간씩 훈련할 것이다.`}
+        {Template`나는 ${<><FontStyles.Br/><Input placeholder={`예)프로그래밍`} type={`text`} getText={setGoal}/><FontStyles.Br/></>} 전문가가 될 것이다.`}
+        {Template`그래서 앞으로 매일 하루에 ${<><FontStyles.Br/><Input placeholder={`예) 5`} type={`number`} getText={setTime}/><FontStyles.Br/></>} 시간씩 훈련할 것이다.`}
         <DivButtons>
           <Button variant={'primary'} onSubmit={handleClick}>나는 며칠 동안 훈련을 해야<FontStyles.Br/> 1만 시간이 될까?</Button>
           <img src="pointer.svg" alt=""/>
